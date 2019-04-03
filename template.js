702c8681e2386635479b2cbed5da04fd9d91668a
glbb666 <menghuangjingy@126.com>
2019-04-03 21:48:11 +0800

    .

--- template.js
+++ template.js
@@ -1,17 +1,5 @@
-function SaferHTML(templateData) {
-  let s = templateData[0];
-  for (let i = 1; i < arguments.length; i++) {
-    let arg = String(arguments[i]);
-
-    // Escape special characters in the substitution.
-    s += arg.replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;");
-
-    // Don't escape special characters in the template.
-    s += templateData[i];
-  }
-  return s;
+function tag(strs) {
+  strs[0] === undefined
+  console.log(strs.raw[0])
 }
-let sender = '<script>alert("abc")</script>'; // 恶意代码
-let message = SaferHTML`<p>${sender} has sent you a message.</p>`;
+console.log(tag`\unicode and \u{55}`)
