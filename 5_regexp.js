db6d9a063eacfff3d92224281b0a921f6146d66e
glbb666 <menghuangjingy@126.com>
2019-04-03 22:59:13 +0800

    .

---
+++ 5_regexp.js
@@ -0,0 +1,17 @@
+// 正确返回字符串长度的函数
+function codePointLength(text){
+    let result = text.match(/\s\S/gu)
+    return result?result.length:0;
+}
+var s = '𠮷𠮷';
+// 单单一个y修饰符对match方法，只能返回第一个匹配，必须与g修饰符联用，才能返回所有匹配。
+
+// ES5 的 source 属性
+// 返回正则表达式的正文
+/abc/ig.source
+// "abc"
+
+// ES6 的 flags 属性
+// 返回正则表达式的修饰符
+/abc/ig.flags
+// 'gi'
