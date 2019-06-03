2ec3116abc96e7f0a17970e344f83b27f4ef5dfb
glbb666 <menghuangjingy@126.com>
2019-06-03 22:40:02 +0800

    。

--- 11_promise.js
+++ 11_promise.js
@@ -10,14 +10,32 @@
 //   p2
 //     .then((value)=> value)
 //     .then(result => console.log(result))  
-const promises = [2, 3, 5, 7, 11, 13].map(function (id) {
-  console.log(1);
-  return id+1;
-});
+// const promises = [2, 3, 5, 7, 11, 13].map(function (id) {
+//   console.log(1);
+//   return id+1;
+// });
 
-Promise.all(promises).then(function (posts) {
-  console.log(2)
-  console.log(posts)
-}).catch(function(reason){
-  // ...
+// Promise.all(promises).then(function (posts) {
+//   console.log(2)
+//   console.log(posts)
+// }).catch(function(reason){
+//   // ...
+// });
+const p1 = new Promise(function (resolve, reject) {
+  setTimeout(() => reject(new Error('fail')), 3000)
+})
+ 
+const p2 = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(p1), 1000)
+})
+
+p2
+  .then(result => {
+console.log("成功");
+console.log("p2:",p2);
+console.log("p1:",result)})
+  .catch(error =>{ 
+console.log("失败");
+console.log("p2",p2);
+console.log("p1",p1);
 });
